c53ad84ba5895d725b4109dd7466383a0db13e26
Abdul-Rashed <122110062+Abdul-Rashed@users.noreply.github.com>
2023-06-02 19:08:58 +0530

    Done with development

---
+++ src/omniFinanceExtractMail.js
@@ -0,0 +1,111 @@
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+const simpleParser = require("mailparser").simpleParser;
+const moment = require('moment');
+
+
+
+module.exports.handler = async (event) => {
+    try {
+        // console.info(event.Records[0].s3.object.key);
+        // const bucket = "omni-finance-receive-dev";
+        // const key = "f6p4pg6qketn3jnlk9fk09gdema21g7t2dnvrgo1";
+        const bucket = event.Records[0].s3.bucket.name;
+        const key = event.Records[0].s3.object.key;
+        const s3Data = await getS3Data(bucket, key);
+        await parseMail(s3Data);
+    } catch (err) {
+        console.log("handler:error", err);
+    }
+    console.log("end of the code");
+};
+
+//Getting the object from s3 bucket to fetch the attachments.
+const getS3Data = (bucket, key) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const params = {
+                Bucket: bucket,
+                Key: key,
+            };
+            const data = await s3.getObject(params).promise();
+            console.log("getS3Data:", data);
+            resolve(data);
+        } catch (error) {
+            console.log("getS3Data:error", error);
+            reject(error);
+        }
+    });
+};
+
+//Fetching mail attachments from the copy of mail and converting buffer to xlsx
+const parseMail = async (data) => {
+    try {
+        const mailData = await simpleParser(data.Body);
+        console.log("attachment:", mailData.attachments.length, mailData.attachments);
+        for (let index = 0; index < mailData.attachments.length; index++) {
+            const element = mailData.attachments[index];
+            console.log("element.filename:", element.filename);
+            if (element.filename.includes(".csv")) {
+                console.log("element.content:", element.content);
+                const outputFilename = element.filename
+                const fileContent = element.content
+                await uploadFileToS3(outputFilename, fileContent);
+            }
+        }
+    } catch (err) {
+        console.log("parseMail:error", err);
+    }
+};
+
+
+//Uploading specific files to specific folders in  S3
+async function uploadFileToS3(outputFilename, fileContent) {
+    console.log("uploadFileToS3")
+    const filename = outputFilename.slice(0, -4) + "_" + moment().format('YYYY-MM-DD_HH:mm:ss') + ".csv"
+    console.log(filename)
+    try {
+        if (outputFilename.includes("Open Invoices-RM")) {
+            await s3
+                .upload({
+                    Bucket: process.env.S3_BUCKET_FOLDER_OPEN_INVOICE_RM,
+                    Key: outputFilename,
+                    Body: fileContent,
+                    ContentType: "application/octet-stream",
+                })
+                .promise();
+        } else if (outputFilename.includes("")) {
+            await s3
+                .upload({
+                    Bucket: process.env.S3_BUCKET_FOLDER_AP_TRANSCTIONS,
+                    Key: outputFilename,
+                    Body: fileContent,
+                    ContentType: "application/octet-stream",
+                })
+                .promise();
+        } else if (outputFilename.includes("")) {
+            await s3
+                .upload({
+                    Bucket: process.env.S3_BUCKET_FOLDER_VENDOR_TYPE,
+                    Key: outputFilename,
+                    Body: fileContent,
+                    ContentType: "application/octet-stream",
+                })
+                .promise();
+        } else if (outputFilename.includes("")) {
+            await s3
+                .upload({
+                    Bucket: process.env.S3_BUCKET_FOLDER_CRITICAL_SUPPLIERS,
+                    Key: outputFilename,
+                    Body: fileContent,
+                    ContentType: "application/octet-stream",
+                })
+                .promise();
+        }
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+
+
