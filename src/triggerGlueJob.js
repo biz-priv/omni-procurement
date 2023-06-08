086423d4c8a5e747e212e9dd7ff9f29d6e472de8
Abdul-Rashed <122110062+Abdul-Rashed@users.noreply.github.com>
2023-06-08 18:54:44 +0530

    commit

---
+++ src/triggerGlueJob.js
@@ -0,0 +1,13 @@
+const AWS = require('aws-sdk');
+const glue = new AWS.Glue();
+
+module.exports.handler = async (event, context) => {
+    try {
+        const jobName = 'sample';
+        const startJobResponse = await glue.startJobRun({ JobName: jobName }).promise();
+        console.log('Glue job started successfully:', startJobResponse.JobRunId);
+        return 'Glue job triggered successfully';
+    } catch (error) {
+        console.error('Failed to start the Glue job:', error);
+    }
+};
